64f5c8cca652c8b8993738498995d78866bf82f9
LarsAstrom <astrom.lars@telia.com>
2019-04-27 13:39:19 +0100

    sorttarg

--- naive.py
+++ naive.py
@@ -9,7 +9,8 @@ def solve(seed, inp, log):
     # TODO: Solve the problem
     random.seed(seed)
     ns = parse(inp)
-    cur_comp = 0
+    uncomps = [i for i in range(ns.C)]
+    #cur_comp = 0
     comp_files = [[False]*ns.C for _ in range(ns.S)]
     repl = defaultdict(list)
     nxt_avail_time = [0]*ns.S
@@ -21,17 +22,21 @@ def solve(seed, inp, log):
             if not comp_files[s][dep]: return False
         return True
     for t in range(MAX_T+1):
+        print 'Cur time: {}, Max time: {}, Uncompiled files {}'.format(t,MAX_T,len(uncomps))
         for rep in repl[t]:
             for s in range(ns.S):
                 comp_files[s][rep] = True
         for s in range(ns.S):
+            if t<nxt_avail_time[s]: continue
             if t==nxt_avail_time[s]: comp_files[s][cur_comp_on_server[s]] = True
-            if t>=nxt_avail_time[s] and is_compable(ns.compilable[cur_comp],s):
-                comp = ns.compilable[cur_comp]
-                cur_comp += 1
-                nxt_avail_time[s] = t + comp.c
-                repl[t+comp.c+comp.r].append(comp.i)
-                out.append((comp.name,s))
+            for comp in uncomps:
+                if is_compable(ns.compilable[comp],s):
+                    uncomps.remove(comp)
+                    comp = ns.compilable[comp]
+                    nxt_avail_time[s] = t + comp.c
+                    repl[t+comp.c+comp.r].append(comp.i)
+                    out.append((comp.name,s))
+                    break
     print len(out)
     out2 = [str(len(out))] + [' '.join(map(str,o)) for o in out]
     return '\n'.join(out2)
