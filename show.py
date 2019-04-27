d33e919ec954eca61b1524e3357a5db0ab0d7bc2
Bjorn <bjorn.in.magnusson@gmail.com>
2019-04-27 12:39:16 +0200

    show2

--- show.py
+++ show.py
@@ -7,10 +7,8 @@ import sys
 # If needed write a simple parser which returns ns.
 # If simple parser should be used run the program with extra argument ('sp')
 def test(inp):
-    sumg = 0
-    for s,d,g in itr[2*ns.C+1:2*ns.C+1+ns.T]:
-        sumq+=int(g)
-    print "C: {}, T: {}, S: {}, sumG: {}".format(ns.C,ns.T,ns.S,sumg)
+    ns = simple_parse(inp)
+    print "C: {}, T: {}, S: {}, sumG: {}".format(ns.C,ns.T,ns.S,ns.sumg)
     # TODO: Write the tests to print given inp.
     # Use parse from score, or write your own (simple_parse)
 
@@ -18,6 +16,10 @@ def simple_parse(inp):
     itr = inp.split('\n')
     ns = argparse.Namespace()
     ns.C, ns.T, ns.S = map(int,itr[0].split())
+    ns.sumg = 0
+    for line in itr[2*ns.C+1:2*ns.C+1+ns.T]:
+        s,d,g = line.split()
+        ns.sumg+=int(g)
     return ns
 
 if __name__=='__main__':
